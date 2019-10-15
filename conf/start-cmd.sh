eb2ab752ad92e5a068ebf64cf502708e52340dcc
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-10-15 14:08:56 +0300

    🐳 Update a Docker control start up order.

--- conf/start-cmd.sh
+++ conf/start-cmd.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Backend Dockerfile start command.
 
+# Build depends_on working alternative to docker-compose.
+# Relation: PostgreSQL <- Backend <- Nginx
+# https://docs.docker.com/compose/startup-order/
+while ! nc -z postgresql 5432; do
+    echo "Waiting a few moments for the postgres database."
+    sleep 3
+done
+
 # Run migrations for development but for the production, it's better to
 # decouple this functionality to be performed manually by the DevOps team to
 # avoid parallel migrations that lead to breakage and mental coupling.
